765bea36a494bf0edec9f217020767cdb0db3361
harolp <haperezj@gmail.com>
2021-10-12 21:54:49 -0300

    creacion navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,9 @@
+const NavBar = () => {
+    return(
+        <nav className='nav'>
+            <div className='title'>
+                <h1>COMPONENTES SHOP</h1>
+            </div>
+        </nav>
+    )
+}
